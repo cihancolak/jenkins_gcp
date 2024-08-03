e2e71dffeb5067af248c76b086479e05bbdbb742
Cihan <47649049+cihancolak@users.noreply.github.com>
2024-08-03 18:04:42 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "my-bucket" {
   name                     = "jenkins-1-project"
   project                  = "jumpbox-427107"
-  location                 = "asia-south2-a"
+  location                 = "asia"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
