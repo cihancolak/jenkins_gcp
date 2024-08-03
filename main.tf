836cabdd4fe102b3ab57756f82cee6bf15cda100
Cihan <47649049+cihancolak@users.noreply.github.com>
2024-08-03 18:38:35 +0300

    Update main.tf for  jenkins webhook trigger

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "google_storage_bucket" "my-bucket" {
   public_access_prevention = "enforced"
 }
 
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "jenkins-1-project2"
+  project                  = "jumpbox-427107"
+  location                 = "asia"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+
+
